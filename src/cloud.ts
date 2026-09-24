b3920fedf8451ec21459667288f21e8f8bc40792
sdsl-fellow <sdsl.fellow@gmail.com>
2026-09-24 18:22:05 +0900

    Connect game to v17 tutorial batch deployment

--- src/cloud.ts
+++ src/cloud.ts
@@ -3,7 +3,7 @@ import type {Save} from './save';
 import {normalizeItemSave,toCloudItemSave} from './item-save.mjs';
 import {fromStoredSave,toStoredSave} from './completion-save.mjs';
 
-export const CLOUD_API_URL='https://script.google.com/macros/s/AKfycbyGf4dGKwe4hBteQkWSMRnaZzE4PTYn05xXEnTY90Y6EkEzJssJ9n0aCxQibQaE2v77/exec';
+export const CLOUD_API_URL='https://script.google.com/macros/s/AKfycbwt_pMjxNTJ20FV1n2QbFbXKtejRhAncvmuJNYAZFjeqwT4zE6WVAgzrIXhYqGHGRxq/exec';
 const CLOUD_AUTH_KEY='doping-heroes:cloud-auth:v1';
 
 export type CloudSession={studentId:string;token:string;revision:number};
